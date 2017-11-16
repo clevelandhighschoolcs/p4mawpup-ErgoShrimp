e12ec4b5fd7898a14bdaa46e64fc799117451e56
Nathan Jesudason <nathan.jesudason@gmail.com>
2017-11-16 14:13:49 -0800

    Added checking for file change
    currently doesn't work

--- scrape.py
+++ scrape.py
@@ -2,8 +2,13 @@ import urllib2
 from bs4 import BeautifulSoup
 import time
 
+#Updating soup
+
+
+
 
 def main():
+    #Updates Page
     blog_page = 'https://nathan440.wordpress.com/'
     webUrl = urllib2.urlopen(blog_page)
     if (webUrl.getcode() == 200):
@@ -12,27 +17,32 @@ def main():
         name = name_box.text.strip()
         print(name)
         f = open("textfile.txt","w+")
-        f.write(str(soup))
+        f.write(name)
+        fileContent = f.read()
         f.close()
+        time.sleep(5)
+    #Checks for change
+        soup = BeautifulSoup(webUrl, 'html.parser')
+        name_box = soup.find('div', attrs={'class': 'entry-meta'})
+        name2 = name_box.text.strip()
+        if(name2) == (fileContent):
+            print("There has been no change!")
+        else:
+            print("There has been a change!")
         print("One scrape has passed")
         time.sleep(5)
     else:
         print("unable to access website")
-        print("One scrape has passed")
-        time.sleep(5)
-	#f = open("textfile.txt","w+")
-	#data = webUrl.read()
-	#if(str(Predata) == str(data)):
-	#	print("There has been no change!")
-	#else:
-	#	print("There has been a change!")
-	#f.write(Predata)
-	#f.close()
+
 
 
 print("Welcome to Nathan's Web scraper")
 time.sleep(1)
 print("Please note that the script runs forever until you press ctrl+c")
 time.sleep(1)
+"""
+blog_page = 'https://nathan440.wordpress.com/'
+webUrl = urllib2.urlopen(blog_page)
+"""
 while True:
     main()
