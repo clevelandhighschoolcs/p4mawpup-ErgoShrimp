0260ccd714db0c420377e4c26daa8d7de4a133b4
Nathan Jesudason <nathan.jesudason@gmail.com>
2017-12-11 15:05:06 -0800

    add twilio

--- scrape.py
+++ scrape.py
@@ -1,7 +1,7 @@
 import urllib2
 from bs4 import BeautifulSoup
 import time
-
+from twilio.rest import Client
 #Updating soup
 
 '''
@@ -15,7 +15,7 @@ Things to do:
 
 
 
-
+test = False
 
 def blog_update():
     webUrl = urllib2.urlopen(url_input)
@@ -63,6 +63,22 @@ try:
 except Exception:
     print("Request either nothing or not understand. Using Nathan's Unity Blog")
     url_input = 'https://nathan440.wordpress.com/'
+while (test == False):
+    my_phone_number = raw_input("input phone number for twilio to text (just numbers)")
+    twilio_phone_number = raw_input("input phone number that twilio texts from (just numbers)")
+    account_sid = raw_input("input account_sid")
+    auth_token = raw_input("input auth_token")
+    try:
+        client = Client(account_sid, auth_token)
+        client.messages.create(
+            body='Test!',
+            to=my_phone_number,
+            from_=twilio_phone_number
+        )
+        test = True
+    except Exception:
+        print "Test message didn't work!"
+
 if (url_input == "https://nathan440.wordpress.com/"):
     scrape = blog_update()
 else:
@@ -82,5 +98,11 @@ while True:
         print("There has been no change!")
     else:
         print("There has been a change!")
+        client = Client(account_sid, auth_token)
+        client.messages.create(
+            body='There has been a change!',
+            to=my_phone_number,
+            from_=twilio_phone_number
+        )
     OlderScrape = scrape
     time.sleep(float(time_input))
